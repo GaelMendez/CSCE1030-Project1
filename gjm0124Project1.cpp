f58ab2a0428b8d43dd24e0a3401e5e8b644117d5
Gael Mendez <gaeljmendez2090@gmail.com>
2025-03-01 18:41:35 -0600

    Project Finished

--- gjm0124Project1.cpp
+++ gjm0124Project1.cpp
@@ -1,3 +1,6 @@
+//People that helped make this project - Ryan Griffin rcg0149 RyanGriffin3@my.unt.edu
+
+
 #include <iostream>
 #include <cstdlib>
 #include <cctype>
@@ -11,26 +14,90 @@ int main(){
 
     //Initialize variables
     int points = 100;
-    int rand1 = 150;
-    int rand2 = 300;
+    int rand1;
+    int rand2;
     string userName;
     int getOper;
     int counter = 0;
     int correct_answer = 0;
     bool playAgain = true;
     char YON = ' ';
-
+    bool choice;
+    int i;
 
     enum OPERATION{ADD = 1,SUBTRACT = 2, DISPLAY = 3, GIVEUP = 4, EXIT = 5};
     OPERATION operation;
 
-    cout << "Enter your name: ";
-    getline(cin, userName);
+    srand(time(0)); // declaring the seed for the random number generator
+
+    string caption = "+-------------------------------------------------------+\n"
+                    "|\tComputer Science and Engineering\t\t|\n"
+                    "|\tCSCE 1030 - Computer Science I\t\t\t|\n"
+                    "|\tGael Mendez  gjm0124 gaelmendez@my.unt.edu\t|\n"
+                    "--------------------------------------------------------+\n";
+
+    cout << caption << endl;
+
+    do
+    {
+ 
+        choice = true;
+
+        cout << "Enter your name: ";
+        getline(cin, userName);
+
+        for (i = 0; i < userName.size(); ++i)
+        {
+            if (!(isalpha(userName.at(i)) || isspace(userName.at(i)))) // checks each string and make sure there is only alphabet letters or spaces
+            {
+                choice = false; // if its false break out of the loop
+                break;
+            }
+        }
+
+        if (!(choice)) // if false we will output the message
+        {
+            cout << "Your name can only have alphabets or spaces. Enter again." << endl; // if its false you must keep on entering until its true
+        }
+
+    } while (!choice);
+
+    bool word = true; // check every first word that the user enters
+    for (i = 0; i < userName.size(); ++i)
+    {
+        if (isalpha(userName.at(i)))
+        {
+            if (word)
+            {
+                userName.at(i) = toupper(userName.at(i)); // makes sure the first letter of each word will be capitalized
+                word = false;
+            }
+            else
+            {
+                userName.at(i) = tolower(userName.at(i)); // the rest of the remaining letters will be lowercase
+            }
+        }
+        else
+        {
+            word = true;
+        }
+    }
+    cout << "Welcome " << userName << endl;
+
+    do
+    {
+        rand1 = rand() % (150 - 100 + 1) + 100; // generating the random numbers for both variables
+        rand2 = rand() % (150 - 100 + 1) + 100;
+    } while (rand1 >= rand2);
+
+    cout << "Your two random numbers have been generated " << endl;
+
+    
 
     
 
     //Making a multistring output to the screen
-    string OperationMultiString = "1.ADD\n"
+    string OperationMultiString = "\n1.ADD\n"
                             "2.Subtract\n"
                             "3.DISPLAY\n"
                             "4.GIVEUP\n"
@@ -84,7 +151,7 @@ int main(){
             // Subtracts the two random numbers and checks if the user guess is an absolute difference less than 5
             case SUBTRACT:
                 int guessDiff;
-                cout << " Enter your guess diference: ";
+                cout << "Enter your guess diference: ";
                 cin >> guessDiff;
 
                 correct_answer = rand1 - rand2;
@@ -105,9 +172,14 @@ int main(){
                         cout << "Goodbye, " << userName << endl;
                         playAgain = false;
                     }
+                }else{
+                    cout << "Wrong guess" << endl;
+                    points--;
+                    cout << "Your updated points are: " << points << "\n" << endl; 
                 }
 
             break;
+
                 /*
                 Displays the first random number and if he asks again we tell the user that he or she already
                 has displayed the first number, then print the first number agan
@@ -116,8 +188,10 @@ int main(){
 
             case DISPLAY:
                 if(counter == 0){
-                    cout << "The first random number is: " << rand1 << endl;
                     points -= 3;
+                    cout << "Your final points are: " << points << endl;
+                    cout << "The first random number is: " << rand1 << endl;
+                    
                 }else{
                     cout << "You have already displayed the first number." << endl;
                     cout << "The first random number is: " << rand1 << endl;
@@ -125,17 +199,41 @@ int main(){
                 counter++;
                 break;
 
+                /*
+                The user gives up and is being promted if he wants to play again.
+                */
+
             case GIVEUP:
+
+                cout << "Do you want to restart the game? Y/N: ";
+                cin >> YON;
+
+                if(toupper(YON) == 'Y'){
+                    playAgain = true;
+                    cout << " Your two numbers have been generated." << endl;
+                }else if (toupper(YON) == 'N'){
+                    
+                    cout << "Your final points are: " << points << endl;
+                    cout << "Goodbye, " << userName << endl;
+                    playAgain = false;
+                }
+
             break;
+
+            // If the user tries to exit the program, the final points are displayed and the program ends.
+
             case EXIT:
                 cout << "Your final points are: " << points << endl;
                 cout << "Goodbye, " << userName << endl;
                 playAgain = false;
             break;
+            default:
+            cout << "Invalid choice. Please try again." << endl;
+            break;
+        }
 
-    }
     }while(playAgain);
 
     return 0;    
-}
-
+    }
+    
